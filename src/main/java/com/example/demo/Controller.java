16fb4009eed27687ee2277c4e914058865091ca7
radu.ionita <raduionita@htsg.eu>
2022-09-15 13:57:19 +0300

    update pom

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class Controller {
 
-  @GetMapping
+  @GetMapping("/hello")
   public String hello() {
     return "Hello from Tomcat!";
   }
